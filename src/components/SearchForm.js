3429bc8ce9caa792efc8a324752244ad28462867
George Aldington <georgealdington@gmail.com>
2023-01-27 12:13:59 +0000

    added new search icon

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,15 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { TfiSearch } from "react-icons/tfi";
 import "../styles/SearchForm.css";
 
 function SearchForm({ searchText, setSearchText, onSubmit }) {
   const handleInputChange = (event) => setSearchText(event.target.value);
 
   return (
-    <div className="search-form">
+    <div className="search-form" role="search">
       <input type="text" onChange={handleInputChange} value={searchText} />
       <button type="submit" onClick={onSubmit}>
-        &#128269;
+        <TfiSearch />
       </button>
     </div>
   );
